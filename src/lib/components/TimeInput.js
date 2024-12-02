15e94605aa1e8ec9039ccc320773c748488b8338
hasnain_vertex360 <hasnain@vertex360.io>
2024-12-02 13:35:35 +0500

    add isMobileNeeded

--- src/lib/components/TimeInput.js
+++ src/lib/components/TimeInput.js
@@ -1,7 +1,20 @@
-import React, { useEffect, useState, useRef, memo, useCallback, useMemo } from "react";
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  memo,
+  useCallback,
+  useMemo,
+} from "react";
 import InputTimeHelper from "./InputTimeHelper";
 import AmPmInputHelper from "./AmPmInputHelper";
-import { doubleChar, isOnMobileDevice, getDatePartsByProps, getTimeString, timers } from "./actions";
+import {
+  doubleChar,
+  isOnMobileDevice,
+  getDatePartsByProps,
+  getTimeString,
+  timers,
+} from "./actions";
 import ArrowDown from "./ArrowDown";
 import UnitDropdown from "./UnitDropdown";
 
@@ -16,46 +29,68 @@ function TimeInput(props) {
     eachInputDropdown,
     manuallyDisplayDropdown,
     fullTimeDropdown,
+    isMobileNeeded,
   } = props;
   const dateParts = getDatePartsByProps(value, hour12Format);
   const [time, setTime] = useState(dateParts);
-  const [isMobile, setIsMobile] = useState(isOnMobileDevice());
+  const [isMobile, setIsMobile] = useState(isMobileNeeded);
   const hourRef = useRef(null);
   const minuteRef = useRef(null);
   const amPmRef = useRef(null);
 
-  const setHour = useCallback((newHour,other = {}) => setTime((t) => ({ ...t, hour: newHour , ...other })), [setTime]);
-  const setMinutes = useCallback((newMinute, other = {}) => setTime((t) => ({ ...t, minute: newMinute , ...other })), [setTime]);
-  const setAmPM = useCallback((newAmPm,other = {}) => setTime((t) => ({ ...t, amPm: newAmPm , ...other })), [setTime]);
+  const setHour = useCallback(
+    (newHour, other = {}) =>
+      setTime((t) => ({ ...t, hour: newHour, ...other })),
+    [setTime]
+  );
+  const setMinutes = useCallback(
+    (newMinute, other = {}) =>
+      setTime((t) => ({ ...t, minute: newMinute, ...other })),
+    [setTime]
+  );
+  const setAmPM = useCallback(
+    (newAmPm, other = {}) =>
+      setTime((t) => ({ ...t, amPm: newAmPm, ...other })),
+    [setTime]
+  );
 
-  const hourRange = useMemo(()=>hour12Format ? { start: 1, end: 12 } : { start: 0, end: 23 },[hour12Format]);
-  const focusOnMinute = useCallback(() => focusOn(minuteRef),[]);
-  const blurOnMinute = useCallback(() => blurOn(minuteRef),[]);
+  const hourRange = useMemo(
+    () => (hour12Format ? { start: 1, end: 12 } : { start: 0, end: 23 }),
+    [hour12Format]
+  );
+  const focusOnMinute = useCallback(() => focusOn(minuteRef), []);
+  const blurOnMinute = useCallback(() => blurOn(minuteRef), []);
   const focusOnHour = useCallback(() => focusOn(hourRef), []);
   const focusOnAmPm = useCallback(() => focusOn(amPmRef), []);
   const blurOnAmPm = useCallback(() => blurOn(amPmRef), []);
 
-  const toggleAmPm = useCallback((other) => setAmPM(time?.amPm === "AM" ? "PM" : "AM", other), [setAmPM, time?.amPm]);
-  const updateTouchDevice = () => setIsMobile(isOnMobileDevice());
+  const toggleAmPm = useCallback(
+    (other) => setAmPM(time?.amPm === "AM" ? "PM" : "AM", other),
+    [setAmPM, time?.amPm]
+  );
+  const updateTouchDevice = () => setIsMobile(isMobileNeeded);
 
-const setTimeHourString = useCallback(
-  (value) => {
-    const dateParts = getDatePartsByProps(value.replace(/ /g, ""), hour12Format);
-    setHour(dateParts.hour);
-    setMinutes(dateParts.minute);
-    setAmPM(dateParts.amPm);
-    if (value.toLowerCase().includes("am")) {
-      setAmPM("AM");
-    } else if (value.toLowerCase().includes("pm")) {
-      setAmPM("PM");
-    }
-  },
-  [hour12Format]
-);
+  const setTimeHourString = useCallback(
+    (value) => {
+      const dateParts = getDatePartsByProps(
+        value.replace(/ /g, ""),
+        hour12Format
+      );
+      setHour(dateParts.hour);
+      setMinutes(dateParts.minute);
+      setAmPM(dateParts.amPm);
+      if (value.toLowerCase().includes("am")) {
+        setAmPM("AM");
+      } else if (value.toLowerCase().includes("pm")) {
+        setAmPM("PM");
+      }
+    },
+    [hour12Format]
+  );
 
   useEffect(() => {
-    const {hour, minute, amPm} = time || {};
-    const dateString=getTimeString(hour, minute, amPm, hour12Format);
+    const { hour, minute, amPm } = time || {};
+    const dateString = getTimeString(hour, minute, amPm, hour12Format);
     onChangeEveryFormat && onChangeEveryFormat(dateString);
     if (hour !== "" && minute !== "" && !isMobile) {
       onChange && onChange(dateString);
@@ -83,8 +118,10 @@ const setTimeHourString = useCallback(
   const sameInputProps = { ...amPmInputProps, allowDelete, placeholder: "- -" };
 
   return (
-    <div className="react-time-input-picker-wrapper">
-      <div className={`react-time-input-picker ${disabled ? "is-disabled" : ""}`}>
+    <div className='react-time-input-picker-wrapper'>
+      <div
+        className={`react-time-input-picker ${disabled ? "is-disabled" : ""}`}
+      >
         {isMobile ? (
           <MobileInput value={value} onChange={onChange} />
         ) : (
@@ -108,7 +145,7 @@ const setTimeHourString = useCallback(
               range={minuteRange}
             />
             {hour12Format && (
-              <div className="inputWrapper">
+              <div className='inputWrapper'>
                 <AmPmInputHelper
                   {...amPmInputProps}
                   inputRef={amPmRef}
@@ -121,8 +158,18 @@ const setTimeHourString = useCallback(
               </div>
             )}
             <Options
-              timeString={getTimeString(time?.hour, time?.minute, time?.amPm, hour12Format)}
-              {...{ hour12Format, fullTimeDropdown, manuallyDisplayDropdown, setTimeHourString }}
+              timeString={getTimeString(
+                time?.hour,
+                time?.minute,
+                time?.amPm,
+                hour12Format
+              )}
+              {...{
+                hour12Format,
+                fullTimeDropdown,
+                manuallyDisplayDropdown,
+                setTimeHourString,
+              }}
             />
           </React.Fragment>
         )}
@@ -136,7 +183,10 @@ const blurOn = (ref) => ref?.current?.blur?.();
 
 const format24Data = new Array(24)
   .fill("")
-  .map((h, index) => [`${doubleChar(index)} : 00`, [`${doubleChar(index)} : 30`]])
+  .map((h, index) => [
+    `${doubleChar(index)} : 00`,
+    [`${doubleChar(index)} : 30`],
+  ])
   .flat(2);
 let format12Data = [...format24Data];
 format12Data.forEach((hour, index) => {
@@ -149,13 +199,22 @@ format12Data.forEach((hour, index) => {
   } else if (hourInNumber < 12) {
     format12Data[index] = `${hour}  AM`;
   } else {
-    format12Data[index] = `${doubleChar(hourInNumber - 12)} : ${doubleCharMinutes}  PM`;
+    format12Data[index] = `${doubleChar(
+      hourInNumber - 12
+    )} : ${doubleCharMinutes}  PM`;
   }
 });
 
-const Options = memo(props => {
-  const { hour12Format, fullTimeDropdown, manuallyDisplayDropdown, setTimeHourString,timeString } = props;
-  const [fullTimeDropdownVisibility, setFullTimeDropdownVisibility] = useState(false);
+const Options = memo((props) => {
+  const {
+    hour12Format,
+    fullTimeDropdown,
+    manuallyDisplayDropdown,
+    setTimeHourString,
+    timeString,
+  } = props;
+  const [fullTimeDropdownVisibility, setFullTimeDropdownVisibility] =
+    useState(false);
 
   useEffect(() => {
     const hideDropdown = (e) => setFullTimeDropdownVisibility(false);
@@ -182,14 +241,14 @@ const Options = memo(props => {
           <ArrowDown onClick={onArrowDown} />
         </div>
       )}
-      <div className="fullTime__wrapper">
+      <div className='fullTime__wrapper'>
         <UnitDropdown
           fullTimeDropdownVisibility
           data={hour12Format ? format12Data : format24Data}
           shouldDisplay={fullTimeDropdown && fullTimeDropdownVisibility}
           manuallyDisplayDropdown={manuallyDisplayDropdown}
-          type="notRange"
-          className="fullTime"
+          type='notRange'
+          className='fullTime'
           hour12Format={hour12Format}
           {...{
             value: timeString,
@@ -201,18 +260,18 @@ const Options = memo(props => {
       </div>
     </React.Fragment>
   );
-})
+});
 
 const minuteRange = { start: 0, end: 59 };
 
-const MobileInput = memo(props => {
-  const {value,onChange} = props
+const MobileInput = memo((props) => {
+  const { value, onChange } = props;
   const [valueMobile, setValueMobile] = useState(value);
 
   return (
-    <div className="input-time-mobile">
+    <div className='input-time-mobile'>
       <input
-        type="time"
+        type='time'
         value={valueMobile}
         onChange={(e) => {
           setValueMobile(e.target.value);
@@ -221,15 +280,16 @@ const MobileInput = memo(props => {
       />
     </div>
   );
-})
+});
 
 TimeInput.defaultProps = {
-  hour12Format:false,
-  disabled:false,
-  allowDelete:false,
-  eachInputDropdown:false,
-  manuallyDisplayDropdown:false,
-  fullTimeDropdown:false,
+  hour12Format: false,
+  disabled: false,
+  allowDelete: false,
+  eachInputDropdown: false,
+  manuallyDisplayDropdown: false,
+  fullTimeDropdown: false,
+  isMobileNeeded: false,
 };
 
 export default React.memo(TimeInput);
